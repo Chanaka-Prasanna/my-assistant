59fd8a4f9832d984debe8fedcd28563c4291c1c5
Chanaka-Prasanna <chanakapinfo@gmail.com>
2025-01-14 14:10:01 +0530

    file structure is changed

---
+++ src/chat_invoke.py
@@ -0,0 +1,12 @@
+from chat import conversational_rag_chain
+
+response = conversational_rag_chain.invoke(
+    {"input": """
+    I need social links of chanaka.
+     """},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)["answer"]
+
+print(response)
